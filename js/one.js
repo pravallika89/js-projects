7e34e8e4fba8a1f8676a6155fdb43ea81b06eed4
pravallika <pravallikaammineni@github.com>
2021-02-22 15:08:33 -0800

    first commit

---
+++ js/one.js
@@ -0,0 +1,18 @@
+let hisName ="prava";
+
+console.log(hisName);
+
+//  *****Arthimetic operators
+
+let total = 1+5;
+console.log('my total is: ', total);
+
+let a= 10;
+let b = '10';
+
+console.log(typeof(a+b));
+
+let value3= 10;
+let value4= "12";
+
+console.log(value3 < value4);
